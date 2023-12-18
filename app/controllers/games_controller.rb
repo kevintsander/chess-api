96b31a57c11126e3197f41f0f64eac218fe7734f
Kevin Sander <kevintsander@gmail.com>
2023-12-17 21:05:27 -0500

    allow promotion game update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,13 +12,13 @@ class GamesController < ApplicationController
 
   def update
     id = params[:id]
-    unit_location = params[:unit_location]
-    move_location = params[:move_location]
     game = Game.find(id)
     game_state = game.game_state
-    unit = game_state.select_actionable_unit(unit_location)
-    action = game_state.select_allowed_action(unit, move_location)
-    game_state.perform_action(action)
+    if %i[unit_location move_location].all? { |p| params.key?(p) }
+      perform_action
+    elsif params.key?(:promote_unit_type)
+      perform_promote
+    end
     game.save
 
     ActionCable.server.broadcast("game_#{game.id}", game.simplified)
@@ -29,5 +29,29 @@ class GamesController < ApplicationController
     game = Game.find(params[:id])
 
     render json: game.simplified
+  end
+
+  private
+
+  def perform_action
+    unit_location = params[:unit_location]
+    move_location = params[:move_location]
+    unit = game_state.select_actionable_unit(unit_location)
+    action = game_state.select_allowed_action(unit, move_location)
+    game_state.perform_action(action)
+  end
+
+  def perform_promote
+    promote_unit_type_name = params[:promote_unit_type]
+    promote_unit_class = select_promoted_unit_class(promote_unit_type_name)
+    promote = game_state.select_promote_action(promote_unit_class)
+    game_state.perform_action(promote)
+  end
+
+  def select_promoted_unit_class(type_name)
+    { 'Queen' => ChessEngine::Units::Queen,
+      'Rook' => ChessEngine::Units::Rook,
+      'Bishop' => ChessEngine::Units::Bishop,
+      'Knight' => ChessEngine::Units::Knight }[type_name.camelize]
   end
 end
