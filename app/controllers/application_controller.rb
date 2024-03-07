1737ef2b3381794b29540c2fa54d7ba9e9c60070
Kevin Sander <kevintsander@gmail.com>
2024-03-06 22:40:10 -0500

    corrected confirmable issue with devise_token_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :configure_permitted_parameters, if: :devise_controller?
   respond_to :json
+  wrap_parameters false
 
   protected
 
