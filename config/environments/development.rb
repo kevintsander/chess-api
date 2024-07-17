a16adfe17e13c48ba829c8dc83f72fd7eed2619a
Kevin Sander <kevintsander@gmail.com>
2024-07-16 22:41:22 -0400

    changed host formatting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,5 +65,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   Rails.application.config.action_cable.allowed_request_origins = ['http://localhost:4200']
 
-  config.hosts = ["https://test-container-app3.thankfulrock-d8e7a63e.northcentralus.azurecontainerapps.io/"]
+  config.hosts << "test-container-app3.thankfulrock-d8e7a63e.northcentralus.azurecontainerapps.io"
 end
