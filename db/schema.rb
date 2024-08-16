a4a4e45b9cbd4c83a162bd1ce25d2b0200ca61a2
Kevin Sander <kevintsander@gmail.com>
2024-08-15 23:13:55 -0400

    updated credentials

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_18_024926) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_18_222460) do
   create_table "games", id: :uuid, default: -> { "newid()" }, force: :cascade do |t|
     t.text "game_state"
     t.datetime "created_at", null: false
