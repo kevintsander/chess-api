8dc8d9250de5619c6bf7f61b83fd86c0593e4754
Kevin Sander <kevintsander@gmail.com>
2024-10-04 08:26:32 -0400

    trying to add credentials true for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,6 +13,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
              headers: :any,
              methods: %i[get post put patch delete options head],
              expose: %w[access-token expiry token-type uid
-                        client]
+                        client],
+             credentials: true
   end
 end
