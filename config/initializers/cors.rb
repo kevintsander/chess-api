37c8bfe1f4846adc5549842a129affe0e3e12a17
Kevin Sander <kevintsander@gmail.com>
2024-10-04 00:06:32 -0400

    regex cors subdomain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4200', 'proud-beach-078fe360f.5.azurestaticapps.net'
+    origins 'localhost:4200', /https:\/\/.*\.azurestaticapps\.net/
 
     resource '*',
              headers: :any,
